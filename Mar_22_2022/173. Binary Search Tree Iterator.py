bf3e0ab858b89c8f07aa54f23e642240eb3145af
fyisfe <fy552005184@Gmail.com>
2022-03-22 23:54:17 +0800

    Day 51: 173. Binary Search Tree Iterator (Optimize)

--- Mar_22_2022/173. Binary Search Tree Iterator.py
+++ Mar_22_2022/173. Binary Search Tree Iterator.py
@@ -6,22 +6,25 @@
 #         self.right = right
 class BSTIterator:
     def __init__(self, root: Optional[TreeNode]):
-        self.inorderArr = []
-        self.traverse(root)
+        self.callStack = []
+        cur = root
+        while cur:
+            self.callStack.append(cur)
+            cur = cur.left
 
     def next(self) -> int:
-        return self.inorderArr.pop(0)
+        if len(self.callStack):
+            ans = self.callStack.pop()
+            cur = ans.right
+
+            while cur:
+                self.callStack.append(cur)
+                cur = cur.left
+
+            return ans.val
 
     def hasNext(self) -> bool:
-        return len(self.inorderArr) != 0
-
-    def traverse(self, node):
-        if node is None:
-            return
-
-        self.traverse(node.left)
-        self.inorderArr.append(node.val)
-        self.traverse(node.right)
+        return len(self.ans)
 
 
 # Your BSTIterator object will be instantiated and called as such:
