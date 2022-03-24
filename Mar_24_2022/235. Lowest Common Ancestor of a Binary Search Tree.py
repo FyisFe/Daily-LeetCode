5d61df8776e4c7178a3617ffa43b66338a24d952
FyisFe <fy552005184@gmail.com>
2022-03-24 10:28:13 +0800

    Day 54: 35. Lowest Common Ancestor of a Binary Search Tree

---
+++ Mar_24_2022/235. Lowest Common Ancestor of a Binary Search Tree.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+class Solution:
+    def lowestCommonAncestor(
+        self, root: "TreeNode", p: "TreeNode", q: "TreeNode"
+    ) -> "TreeNode":
+
+        if root.val == p.val or root.val == q.val:
+            return root
+
+        if root.val > max(p.val, q.val):
+            return self.lowestCommonAncestor(root.left, p, q)
+
+        if root.val < min(p.val, q.val):
+            return self.lowestCommonAncestor(root.right, p, q)
+
+        else:
+            return root
