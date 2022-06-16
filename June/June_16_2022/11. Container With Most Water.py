a083e12c5f5bd3d3fc743b14ccc0125e06040095
fyisfe <fy552005184@gmail.com>
2022-06-16 16:14:57 +0800

    Day 137: 11. Container With Most Water

---
+++ June/June_16_2022/11. Container With Most Water.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        res = 0
+        left, right = 0, len(height) - 1
+
+        while left < right:
+            res = max(res, min(height[left], height[right]) * (right - left))
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+        return res
