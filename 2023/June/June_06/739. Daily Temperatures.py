53878be75b33146fff6ec3794aa81d7e00c7e413
fyisfe <fy552005184@gmail.com>
2023-06-06 23:12:24 -0700

    solving Stack problems

---
+++ 2023/June/June_06/739. Daily Temperatures.py
@@ -0,0 +1,15 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        n = len(temperatures)
+        answer = [0] * n
+        stack = []
+
+        for curr_day, curr_temp in enumerate(temperatures):
+            # Pop until the current day's temperature is not
+            # warmer than the temperature at the top of the stack
+            while stack and temperatures[stack[-1]] < curr_temp:
+                prev_day = stack.pop()
+                answer[prev_day] = curr_day - prev_day
+            stack.append(curr_day)
+
+        return answer
